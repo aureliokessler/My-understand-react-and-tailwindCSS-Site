b4a4d0459c474fc258371064e5712c8974e8e908
Aurelio Kessler <aureliokessler@gmail.com>
2023-01-19 22:13:28 +0100

    add the upsplash images links to support the creators of the pictures I use.

--- src/pages/ImpressumPage.jsx
+++ src/pages/ImpressumPage.jsx
@@ -5,6 +5,11 @@ export default function ImpressumPage() {
         <>
             <div>
                 <div>Impressum</div>
+                <p>Photo from <a href="https://unsplash.com/@doctype?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Aleks Dorohovich</a> on <a href="https://unsplash.com/de/fotos/nJdwUHmaY8A?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </p>
+                <p>
+                    Photo from <a href="https://unsplash.com/@thoughtcatalog?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Thought Catalog</a> on <a href="https://unsplash.com/de/fotos/505eectW54k?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </p>
             </div>
         </>
     );
