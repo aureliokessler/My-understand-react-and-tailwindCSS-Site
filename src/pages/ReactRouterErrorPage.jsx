e8c8524a7489797f1a4dc71729839c89f951197b
Aurelio Kessler <aureliokessler@gmail.com>
2023-01-30 19:38:29 +0100

    a bit better error page design.

--- src/pages/ReactRouterErrorPage.jsx
+++ src/pages/ReactRouterErrorPage.jsx
@@ -5,10 +5,10 @@ export default function ReactRouterErrorPage() {
     console.error(error);
 
     return (
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <h1 className="rounded-lg px-4 py-2 bg-red-600 text-white text-xl">Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
+        <div className="mx-auto max-w-7xl px-8 py-8">
+            <h1 className="font-semibold text-2xl text-gray-700 pb-2">Oops!</h1>
+            <p className="text-sm">Sorry, an unexpected error has occurred.</p>
+            <p className="pt-2 text-gray-700">
                 <i>{error.statusText || error.message}</i>
             </p>
         </div>
