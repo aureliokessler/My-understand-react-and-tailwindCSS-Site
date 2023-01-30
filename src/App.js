62dc6b888eeb677d62525027bd8757aca4960f44
Aurelio Kessler <aureliokessler@gmail.com>
2023-01-30 19:45:02 +0100

    Added the complete test data and pages.

--- src/App.js
+++ src/App.js
@@ -3,59 +3,183 @@ import {
     RouterProvider,
 } from 'react-router-dom'
 
-import Header from './components/Header'
-import Fooder from './components/Fooder'
+import { directoryTree } from './components/core/Functions'
+
+import { Badge } from './components/Badge'
+import HeaderView from './components/HeaderView'
+import FooderView from './components/FooderView'
 
 import HomePage from './pages/HomePage'
-import BlogPage from './pages/BlogPage'
-import ProjectsPage from './pages/ProjectsPage'
-import ImpressumPage from './pages/ImpressumPage'
+import CollectionPage from './pages/CollectionPage'
+import DataProtectionPage from './pages/DataProtectionPage'
 import ContactsPage from './pages/ContactsPage'
 import ReactRouterErrorPage from './pages/ReactRouterErrorPage'
 
 import './App.css';
 
-const navigationData = [
+
+const bannerData = [
+    {
+        title: "ESP-IDF in a Docker",
+        badges: [
+            {
+                text: "projects",
+                bgColorClass: "bg-cyan-600"
+            },
+            {
+                text: "blog",
+                bgColorClass: "bg-purple-700"
+            }
+        ],
+        description: "In hac habitasse platea dictumst. Sed nec venenatis odio. Nulla faucibus ipsum sed faucibus accumsan. Donec rhoncus luctus massa vitae lobortis. Duis consequat, nunc a pretium imperdiet, neque est rhoncus massa, tristique rutrum nisl risus at libero.",
+        createdAt: 1674244305000
+    },
+    {
+        title: "ESP-IDF in a Docker",
+        description: "In hac habitasse platea dictumst. Sed nec venenatis odio. Nulla faucibus ipsum sed faucibus accumsan. Donec rhoncus luctus massa vitae lobortis. Duis consequat, nunc a pretium imperdiet, neque est rhoncus massa, tristique rutrum nisl risus at libero.",
+        createdAt: 1674152802000
+    },
+    {
+        title: "ESP-IDF in a Docker",
+        description: "In hac habitasse platea dictumst. Sed nec venenatis odio. Nulla faucibus ipsum sed faucibus accumsan. Donec rhoncus luctus massa vitae lobortis. Duis consequat, nunc a pretium imperdiet, neque est rhoncus massa, tristique rutrum nisl risus at libero.",
+        createdAt: 1674152802000
+    },
+];
+
+const posts = [
+    {
+        title: "ESP-IDF in a Docker",
+        badges: [
+            {
+                text: "projects",
+                bgColorClass: "bg-cyan-600"
+            },
+            {
+                text: "blog",
+                bgColorClass: "bg-purple-700"
+            }
+        ],
+        description: "In hac habitasse platea dictumst. Sed nec venenatis odio. Nulla faucibus ipsum sed faucibus accumsan. Donec rhoncus luctus massa vitae lobortis. Duis consequat, nunc a pretium imperdiet, neque est rhoncus massa, tristique rutrum nisl risus at libero.",
+        createdAt: 1674244305000
+    },
+    {
+        title: "ESP-IDF in a Docker",
+        description: "In hac habitasse platea dictumst. Sed nec venenatis odio. Nulla faucibus ipsum sed faucibus accumsan. Donec rhoncus luctus massa vitae lobortis. Duis consequat, nunc a pretium imperdiet, neque est rhoncus massa, tristique rutrum nisl risus at libero.",
+        createdAt: 1674152802000
+    },
+    {
+        title: "ESP-IDF in a Docker",
+        description: "In hac habitasse platea dictumst. Sed nec venenatis odio. Nulla faucibus ipsum sed faucibus accumsan. Donec rhoncus luctus massa vitae lobortis. Duis consequat, nunc a pretium imperdiet, neque est rhoncus massa, tristique rutrum nisl risus at libero.",
+        createdAt: 1673634402000
+    },
+    {
+        title: "ESP-IDF in a Docker",
+        description: "In hac habitasse platea dictumst. Sed nec venenatis odio. Nulla faucibus ipsum sed faucibus accumsan. Donec rhoncus luctus massa vitae lobortis. Duis consequat, nunc a pretium imperdiet, neque est rhoncus massa, tristique rutrum nisl risus at libero.",
+        createdAt: 1672527600000
+    },
+    {
+        title: "ESP-IDF in a Docker",
+        description: "In hac habitasse platea dictumst. Sed nec venenatis odio. Nulla faucibus ipsum sed faucibus accumsan. Donec rhoncus luctus massa vitae lobortis. Duis consequat, nunc a pretium imperdiet, neque est rhoncus massa, tristique rutrum nisl risus at libero.",
+        createdAt: 1623015435000
+    },
+];
+
+const testTree = [
+    {
+        type: "directory",
+        name: "2020 Roadmap",
+        children: [
+            {
+                type: "directory",
+                name: "Public GitHub Projects",
+                children: [
+                    {
+                        type: "externLink",
+                        name: "aureliokessler.ch",
+                        url: "github repo"
+                    },
+                    {
+                        type: "externLink",
+                        name: "RavenfalBridgeAPI in PHP",
+                        url: "github repo"
+                    }
+                ]
+            },
+            {
+                type: "directory",
+                name: "Learns",
+                children: [
+                    {
+                        type: "book",
+                        name: "Swift 4.2 Book",
+                        progress: {
+                            sites: 1000,
+                            read: 67
+                        }
+                    },
+                ]
+            },            
+            {
+                type: "directory",
+                name: "Projects",
+                children: [
+                    {
+                        type: "relativLink",
+                        name: "Lawalamp",
+                        url: "#"
+                    },
+                ]
+            },            
+        ]
+    }
+];
+
+const navigationHeader = [
     { name: 'Home', href: '/', current: true },
     { name: 'Blog', href: 'blog', current: false },
     { name: 'Projects', href: 'projects', current: false },
 ]
 
 const navigationFooder = [
-    { name: 'Impressum', href: 'impressum', current: false },
+    { name: 'data protection', href: 'dataprotection', current: false },
     { name: 'Contact', href: 'contacts', current: false },
 ]
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <HomePage />,
+        element: <HomePage bannerData={bannerData} />,
         errorElement: <ReactRouterErrorPage />,
     },
     {
         path: "blog",
-        element: <BlogPage />
+        element: <CollectionPage title="blog" posts={posts} panelRightContent={
+            <>
+                <Badge text="Project" bgColorClass="bg-cyan-600 mr-2" />
+                <Badge text="Project" bgColorClass="bg-cyan-600" />
+            </>
+        } />
     },
     {
         path: "projects",
-        element: <ProjectsPage />
+        element: <CollectionPage title="projects" titleBGColorClass='bg-cyan-600' posts={posts} panelRightContent={directoryTree(testTree)} />
     },
     {
-        path: "impressum",
-        element: <ImpressumPage />
+        path: "dataprotection",
+        element: <DataProtectionPage />
     },
     {
         path: "contacts",
         element: <ContactsPage />
     },
-    ]);
+]);
 
 function App() {
     return (
         <>
-            <Header navigation={navigationData} />
+            <HeaderView navigation={navigationHeader} />
             <RouterProvider router={router} />
-            <Fooder navigationLeft={navigationFooder} socialMediaLinks={[]} />
+            <FooderView navigationLeft={navigationFooder} socialMediaLinks={[]} />
         </>
     );
 }
