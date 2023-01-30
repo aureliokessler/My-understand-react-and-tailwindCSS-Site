f6a9d17c8a75f27bb0425cf9f14dd7b894b2be39
Aurelio Kessler <aureliokessler@gmail.com>
2023-01-30 19:43:36 +0100

    change attribute to react name convention.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,34 +1,31 @@
-import { Banner450p, BannerTo } from '../components/Banner'
+import { Banner450p } from '../components/Banner'
+import { Card } from '../components/Cards'
 
-const bannerTextData = [
-    { text: 'Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat.' },
-];
-
-export default function HomePage() {
+export default function HomePage( { bannerData = []} ) {
     return (
         <>
             <Banner450p imageClass="home-bg" content={
                     <>
-                        <div class="relative flex flex-col lg:flex-row mx-auto lg:max-w-7xl py-3 sm:py-5">
-                            <BannerTo text="first" />
-                            <BannerTo text="second" />
-                            <BannerTo text={bannerTextData[0].text} />
+                        <div className="justify-center items-center flex flex-col py-5 space-y-10 md:flex-row md:space-x-10 md:space-y-0 overflow-hidden">
+                            {bannerData.map( (data, index) => (
+                                <Card key={index} data={data} />
+                            ))}
                         </div>
-                        <div class="relative flex justify-center space-x-2">
-                            <div class="bg-lime-600 border border-lime-400 w-3.5 h-3.5 rounded-full"></div>
-                            <div class="bg-white border border-lime-400 w-3.5 h-3.5 rounded-full"></div>
-                            <div class="bg-white border border-lime-400 w-3.5 h-3.5 rounded-full"></div>
-                            <div class="bg-white border border-lime-400 w-3.5 h-3.5 rounded-full"></div>
+                        <div className="relative flex justify-center space-x-2">
+                            <div className="bg-lime-600 border border-lime-400 w-3.5 h-3.5 rounded-full"></div>
+                            <div className="bg-white border border-lime-400 w-3.5 h-3.5 rounded-full"></div>
+                            <div className="bg-white border border-lime-400 w-3.5 h-3.5 rounded-full"></div>
+                            <div className="bg-white border border-lime-400 w-3.5 h-3.5 rounded-full"></div>
                         </div>
                     </>
                 } />
-            <div class="mx-auto max-w-7xl px-2 py-10 sm:px-6 lg:px-8">
-                <div class="inline rounded-md px-7 py-2 text-2xl font-semibold text-white bg-green-600">ME</div>
-                <h1 class="py-6 text-5xl font-semibold text-gray-600">Aurelio Kessler</h1>
-                <p class="text-gray-600 pb-5">
+            <div className="mx-auto max-w-7xl px-2 py-10 sm:px-6 lg:px-8">
+                <div className="inline rounded-md px-7 py-2 text-2xl font-semibold text-white bg-lime-500">ME</div>
+                <h1 className="py-6 text-5xl font-semibold text-gray-600">Aurelio Kessler</h1>
+                <p className="text-gray-600 pb-5">
                     Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat.
                 </p>
-                <p class="text-gray-600  pb-5">
+                <p className="text-gray-600  pb-5">
                     Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat. Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat.
                 </p>
             </div>
